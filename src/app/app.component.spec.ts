b1116d2e48e7b8d5fa17995fd172b17817406b70
ShadyNagy <info@shadynagy.com>
2021-11-06 01:08:44 +0200

    fix the test.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'calc'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('calc');
+    expect(app.display).toEqual('');
   });
 
   it('should render title', () => {
